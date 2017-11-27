9a61330da502f563367a3daf0f4dd6b3d8e4e90f
Cai <mail@caiwingfield.com>
2017-11-27 22:02:32 +0000

    Add Github link.

--- footer.php
+++ footer.php
@@ -2,8 +2,8 @@
 			<ul>
 				<li>All content seen here &copy; its creator.</li>
 				<li>All code genreating this site is released as <a href="https://creativecommons.org/publicdomain/zero/1.0/">public domain</a>.</li>
-				<li>Source code available <a href="#">on Github</a>.</li>
-				<li>Please report any issues <a href="#">on Github</a>.</li>
+				<li>Source code available <a href="https://github.com/caiw/podcast-feed-viewer">on Github</a>.</li>
+				<li>Please report any issues <a href="https://github.com/caiw/podcast-feed-viewer">on Github</a>.</li>
 				<li>Built and hosted by <a href="http://cai.zone">Cai</a>.</li>
 			</ul>
 		</div>
