c61a9f88369f27a8c30f38f4f7116c220a2162d9
Cai <mail@caiwingfield.com>
2017-11-30 23:03:15 +0000

    Fix up footer links.

--- footer.php
+++ footer.php
@@ -1,9 +1,16 @@
 		<div class="footer">
-			<ul>
-				<li>All content seen here &copy; its creator.</li>
-				<li>All code genreating this site is released as <a href="https://creativecommons.org/publicdomain/zero/1.0/">public domain</a>.</li>
-				<li>Source code available <a href="https://github.com/caiw/podcast-feed-viewer">on Github</a>.</li>
-				<li>Please report any issues <a href="https://github.com/caiw/podcast-feed-viewer">on Github</a>.</li>
-				<li>Built and hosted by <a href="http://cai.zone">Cai</a>.</li>
-			</ul>
+			<div class="meta-links links">
+				<ul>
+					<li>All content seen here is &copy; its creator.</li>
+					<li>All code genreating this site is released as <a href="https://creativecommons.org/publicdomain/zero/1.0/">public domain</a>.</li>
+					<li>Source code available <a href="https://github.com/caiw/podviewer.cai.zone.git">on Github</a>.</li>
+					<li>Please report any issues <a href="https://github.com/caiw/podviewer.cai.zone.git">on Github</a>.</li>
+					<li>Built and hosted by <a href="http://cai.zone">Cai</a>.</li>
+				</ul>
+			</div>
+			<div class="external-links links">
+				<ul>
+					<li>You may also find useful <a href="http://getrssfeed.com">this tool</a> for extracting RSS feeds from iTunes and Soundcloud URLs.</li>
+				</ul>
+			</div>
 		</div>
