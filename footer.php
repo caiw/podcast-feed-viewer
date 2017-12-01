db9af6f16edc487813ff43cdb939f025ca2d1a9e
Cai <mail@caiwingfield.com>
2017-12-01 20:06:59 +0000

    Fixed a typo.

--- footer.php
+++ footer.php
@@ -2,8 +2,7 @@
 			<div class="meta-links links">
 				<ul>
 					<li>All content seen here is &copy; its creator.</li>
-					<li>All code genreating this site is released as <a href="https://creativecommons.org/publicdomain/zero/1.0/">public domain</a>.</li>
-					<li>Source code available <a href="https://github.com/caiw/podviewer.cai.zone.git">on Github</a>.</li>
+					<li>Source code available <a href="https://github.com/caiw/podviewer.cai.zone.git">on Github</a>, released as <a href="https://creativecommons.org/publicdomain/zero/1.0/">public domain</a>.</li>
 					<li>Please report any issues <a href="https://github.com/caiw/podviewer.cai.zone.git">on Github</a>.</li>
 					<li>Built and hosted by <a href="http://cai.zone">Cai</a>.</li>
 				</ul>
